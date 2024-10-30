e85aeee07c4dc9e4670516e358fb2f6fa2566b51
Jase Kraft <jasekraft12@gmail.com>
2024-10-30 08:33:35 -0500

    hide chat on public pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import ChatBubble from './components/ChatBubble';
 const theme = createTheme();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <App />
-      <ChatBubble />
     </ThemeProvider>
   </StrictMode>,
 )
