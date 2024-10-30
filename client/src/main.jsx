bd0434e46a9ae947883d01d9c7e69aeda1c25091
Jase Kraft <jasekraft12@gmail.com>
2024-10-30 08:28:07 -0500

    added chat on the fe

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import ChatBubble from './components/ChatBubble';
 const theme = createTheme();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <App />
+      <ChatBubble />
     </ThemeProvider>
   </StrictMode>,
 )
