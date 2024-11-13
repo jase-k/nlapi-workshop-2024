b88d1e21209ecc07bfe94a4c8f7fe6a9ba0c811e
Jase Kraft <jasekraft12@gmail.com>
2024-11-13 08:15:45 -0600

    recipe put

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -51,14 +51,7 @@ exports.getRecipeById = async (req, res) => {
 
 exports.updateRecipe = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { title, instructions } = req.body;
-    const recipe = await Recipe.findByPk(id);
-    if (!recipe) {
-      return res.status(404).json({ error: 'Recipe not found' });
-    }
-    await recipe.update({ title, instructions });
-    res.status(200).json(recipe);
+    res.status(400).json({ error: 'Not implemented yet' });
   } catch (error) {
     res.status(500).json({ error: `Failed to update recipe: ${error.message}` });
   }
